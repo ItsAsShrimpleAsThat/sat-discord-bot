a12f5f6d599fc11fe627daa7f763d07ea9f62c2e
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-09-04 10:33:08 -0700

    buttons n stuff

--- bot.py
+++ bot.py
@@ -2,15 +2,35 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
-client = commands.Bot(command_prefix = ")", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix = ")", intents=discord.Intents.all())
 
-@client.event
+class ResponseButtons(discord.ui.View):
+    def __init__(self, *, timeout=180):
+        super().__init__(timeout=timeout)
+
+    @discord.ui.button(label="test",style=discord.ButtonStyle.blurple)
+    async def optionA(self, interaction:discord.Interaction, button:discord.ui.Button):
+        await interaction.response.send_message("omg it worked", ephemeral=True)
+
+
+@bot.tree.command(name="sat", description="sat")
+async def sat(interaction: discord.Interaction, value: int):
+    await interaction.response.send_message("hello!", ephemeral=True)
+
+@bot.command()
+async def testbuttons(ctx):
+    await ctx.send("hello", view = ResponseButtons())
+
+@bot.event
 async def on_ready():
     print("bot online!!!")
     try:
-        synced = await client.tree.sync()
+        synced = await bot.tree.sync()
         print(f"Synced {len(synced)} commands")
     except:
         print("ha didn't work lmaooooo")
 
-client.run(discordAPIkey)
+tokenFile = open("token.env", "r")
+token = tokenFile.read()
+tokenFile.close()
+bot.run(token)
