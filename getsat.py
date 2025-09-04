6d556220d653ac9fb6836b84d6c931ec0b492e08
Andrew <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-09-04 11:04:25 -0700

    get questions url from domains you want of questions haahahaa

--- getsat.py
+++ getsat.py
@@ -1,11 +1,28 @@
-def getDomains(information_and_ideas,
-               craft_and_structure,
-               expression_of_ideas,
-               standard_english_conventions,
-               algebra,
-               advanced_math,
-               problem_solving_data_analysis,
-               geometry_and_trig):
+import requests
+
+GET_QUESTIONS_API = "https://practicesat.vercel.app/api/get-questions"
+QUESTION_BY_ID_API = "https://practicesat.vercel.app/api/question-by-id/"
+
+DOMAINS_LOOKUP = { 
+                   0: "", 
+                   1: "INI", 
+                   2: "CAS", 
+                   4: "EOI", 
+                   8: "SEC", 
+                   16: "H", 
+                   32: "P", 
+                   64: "Q", 
+                   128: "S", 
+                }
+
+def getDomains(information_and_ideas:bool,
+               craft_and_structure:bool,
+               expression_of_ideas:bool,
+               standard_english_conventions:bool,
+               algebra:bool,
+               advanced_math:bool,
+               problem_solving_data_analysis:bool,
+               geometry_and_trig:bool):
     num = 0
     num = num | (1 << 0) if information_and_ideas else num
     num = num | (1 << 1) if craft_and_structure else num
@@ -15,6 +32,17 @@ def getDomains(information_and_ideas,
     num = num | (1 << 5) if advanced_math else num
     num = num | (1 << 6) if problem_solving_data_analysis else num
     num = num | (1 << 7) if geometry_and_trig else num
+    return num
 
-def getQuestions():
-    pass
+def getQuestionsRequestURL(domains:int):
+    querystring = ""
+    for i in range(8):
+        masked = domains & (1 << i)
+        domainID = DOMAINS_LOOKUP[masked]
+
+        if(domainID != ""):
+            querystring += domainID + ","
+
+    querystring = querystring.removesuffix(",")
+    return GET_QUESTIONS_API + "?domains=" + querystring
+
